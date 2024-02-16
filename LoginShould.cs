11313b2570598ad7842e5310f7ac64021bf61b71
carloserochav <rochavawave@gmail.com>
2024-02-16 12:09:22 -0800

    Unit Test

---
+++ LoginShould.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using notes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using notes.Repository;
+using System.ComponentModel.DataAnnotations;
+using notes.DTOs;
+namespace notes.Tests
+{
+    public class LoginShould
+    {
+        private readonly DbContextOptions<NotesContext> dbContextOptions;
+
+
+        public LoginShould()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<NotesContext>()
+                 .UseInMemoryDatabase("notes")
+                 .Options;
+        }
+
+
+        [Fact]
+        public async Task ValidateLogin()
+        {
+            //ARRANGE
+            var notesContext = new NotesContext(dbContextOptions);
+            AuthRepository repository = new AuthRepository(notesContext);
+
+            //ACT            
+            var newUser = new Users()
+            {
+                Username = "mark12345",
+                Email = "mark@gmail.com",
+                Password = "123456"
+            };           
+
+            using (notesContext)            {
+
+                //notesContext.Add(newUser);
+                await notesContext.UserModel.AddAsync(newUser);
+                await notesContext.SaveChangesAsync();
+
+                //ACT              
+                var result = await repository.Login(new LoginDto { Email=newUser.Email,Password=newUser.Password});
+
+                //Assert
+                Assert.NotNull(result);
+        }
+
+            
+            
+
+            
+
+            
+        }
+        
+    }
+}
---
+++ RegisterShould.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using notes.Models;
+using notes.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notes.Tests
+{
+    public class RegisterShould
+    {
+
+        // Arrange
+        public readonly DbContextOptions<NotesContext> dbContextOptions;//Set Context   ; We need the context to do operations      
+        //Crear un Context para poder realizar ; Conectar a un abase de datos en memoria        
+        
+
+        public RegisterShould()//Constructor to set database-in-memory ;  Then you can test your repository logic.
+        {
+            dbContextOptions = new DbContextOptionsBuilder<NotesContext>()
+                .UseInMemoryDatabase(databaseName:"notes")
+                .Options;
+        }
+
+        //Method to validate User register
+        [Fact]//Atributo para hacer metodo de Test
+        public async Task ValidateRegister()
+        {
+            //ARRANGE            
+            var notesContext = new NotesContext(dbContextOptions);
+            AuthRepository repository = new AuthRepository(notesContext);
+            var newUser = new Users()
+            {
+                Username = "mat12345",
+                Email = "mat123@gmail.com",
+                Password = "123456"
+            };
+
+            // Act
+            // Perform repository method calls            
+            bool result = await repository.Create(newUser);
+
+            // Assert
+            // Make assertions based on expected behavior            
+            //Assert.Equal(1,await notesContext.)
+            List<Users> users = notesContext.UserModel.ToList();
+            Assert.True(result);
+            //Assert.Single(users);            
+        }
+
+        
+    }
+}
